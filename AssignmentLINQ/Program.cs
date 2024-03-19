aa798bf253e9caeae73c801adb700317e3dea67c
DumbravaM <42375617+dumbrava2001@users.noreply.github.com>
2024-03-19 16:20:41 +0200

    Extract all implementations to concrete methods

--- AssignmentLINQ/Program.cs
+++ AssignmentLINQ/Program.cs
@@ -4,6 +4,98 @@ internal static class Program
 {
     public static void Main(string[] args)
     {
+        var laptopList = GetLaptopList();
+
+        var delegateAndLinQ = new DelegateAndLinQ();
+
+        // using delegate for passing method as object
+        SearchLaptopUsingParsedDelegate(delegateAndLinQ, laptopList);
+
+        // using anonymous method to search laptops
+        Console.WriteLine("\nSearch laptops using anonymous method delegate");
+        SearchLaptopUsingAnonymousMethodDelegate(delegateAndLinQ, laptopList);
+
+        // using lambda expression
+        Console.WriteLine("\nSearch laptops using lambda expression");
+        SearchLaptopUsingLambdaExpression(delegateAndLinQ, laptopList);
+
+        //using Func to pass comparator method
+        Console.WriteLine("\nSearch laptops using Func");
+        SearchLaptopUsingFunc(delegateAndLinQ, laptopList);
+
+        //using extension method to get selling like title for laptop 
+        Console.WriteLine("\nUsing extension method");
+        Console.WriteLine(laptopList[0].GetLaptopSellingTitle());
+
+        // using select and where on collection
+        Console.WriteLine("\nSearch laptops where and select operators");
+        SearchLaptopsUsingWhereAndSelectOperators(laptopList);
+    }
+
+    private static void SearchLaptopsUsingWhereAndSelectOperators(List<Laptop> laptopList)
+    {
+        var asusLaptopsWithMoreThan8Ram = laptopList.Where(laptop =>
+                laptop.Brand.Equals("ASUS", StringComparison.OrdinalIgnoreCase) && laptop.Motherboard.RamCount > 8)
+            .Select(laptop => new
+            {
+                laptop.Motherboard.Brand, laptop.Motherboard.CPU, laptop.Motherboard.RamCount,
+                laptop.Motherboard.SsdSize
+            });
+
+        foreach (var laptop in asusLaptopsWithMoreThan8Ram)
+        {
+            Console.WriteLine($"{laptop.Brand}, {laptop.CPU}, Ram:{laptop.RamCount}, SSD:{laptop.SsdSize}");
+        }
+    }
+
+    private static void SearchLaptopUsingFunc(DelegateAndLinQ delegateAndLinQ, List<Laptop> laptopList)
+    {
+        var laptopsWithLessThan16Ram = delegateAndLinQ.FindLaptopByCriteriaUsingFunc(laptopList,
+            (laptop) => laptop.Motherboard.RamCount < 16);
+
+        DisplayList(laptopsWithLessThan16Ram);
+    }
+
+    private static void SearchLaptopUsingLambdaExpression(DelegateAndLinQ delegateAndLinQ, List<Laptop> laptopList)
+    {
+        DelegateAndLinQ.CompareLaptop msiMotherboardFilter = laptop =>
+            laptop.Motherboard.Brand.Contains("MSI", StringComparison.OrdinalIgnoreCase);
+        var msiMotherboardBasedLaptops =
+            delegateAndLinQ.FindLaptopByCriteriaUsingDelegate(laptopList, msiMotherboardFilter);
+
+        DisplayList(msiMotherboardBasedLaptops);
+    }
+
+    private static void SearchLaptopUsingAnonymousMethodDelegate(DelegateAndLinQ delegateAndLinQ,
+        List<Laptop> laptopList)
+    {
+        DelegateAndLinQ.CompareLaptop ryzenCpuFilter = delegate(Laptop laptop)
+        {
+            return laptop.Motherboard.CPU.Contains("Ryzen", StringComparison.OrdinalIgnoreCase);
+        };
+        var ryzenCpuBasedLaptops = delegateAndLinQ.FindLaptopByCriteriaUsingDelegate(laptopList, ryzenCpuFilter);
+
+        DisplayList(ryzenCpuBasedLaptops);
+    }
+
+    private static void SearchLaptopUsingParsedDelegate(DelegateAndLinQ delegateAndLinQ, List<Laptop> laptopList)
+    {
+        var searchedLaptopList =
+            delegateAndLinQ.FindLaptopByCriteriaUsingDelegate(laptopList, laptop => laptop.Brand.Equals("ASUS"));
+        DisplayList(searchedLaptopList);
+    }
+
+    private static void DisplayList(IEnumerable<Laptop> laptopList)
+    {
+        foreach (var laptop in laptopList)
+        {
+            Console.WriteLine(
+                $"Laptop:{laptop.Brand}, {laptop.DisplaySize} inch\nMotherboard:{laptop.Motherboard.Brand}, {laptop.Motherboard.CPU}, Ram:{laptop.Motherboard.RamCount}, SSD:{laptop.Motherboard.SsdSize}");
+        }
+    }
+
+    private static List<Laptop> GetLaptopList()
+    {
         var laptopList = new List<Laptop>()
         {
             new("ASUS", 14, new Motherboard("ASUS", "Ryzen 7", 24, 512)),
@@ -18,40 +110,6 @@ internal static class Program
             new("ACER", 15.3, new Motherboard("MSI", "Ryzen 3", 8, 512)),
             new("ASUS", 13, new Motherboard("ASUS", "Intel 7", 16, 512)),
         };
-
-        var delegateAndLinQ = new DelegateAndLinQ();
-
-        // using delegate for passing method as object
-        var searchedLaptopList =
-            delegateAndLinQ.FindLaptopByCriteriaUsingDelegate(laptopList, laptop => laptop.Brand.Equals("ASUS"));
-
-        // using anonymous method to search laptops
-        DelegateAndLinQ.CompareLaptop ryzenCpuFilter = delegate(Laptop laptop)
-        {
-            return laptop.Motherboard.CPU.Contains("Ryzen", StringComparison.OrdinalIgnoreCase);
-        };
-        var ryzenCpuBasedLaptops = delegateAndLinQ.FindLaptopByCriteriaUsingDelegate(laptopList, ryzenCpuFilter);
-
-        // using lambda expression and func
-        DelegateAndLinQ.CompareLaptop msiMotherboardFilter = laptop =>
-            laptop.Motherboard.Brand.Contains("MSI", StringComparison.OrdinalIgnoreCase);
-        var msiMotherboardBasedLaptops =
-            delegateAndLinQ.FindLaptopByCriteriaUsingDelegate(laptopList, msiMotherboardFilter);
-
-        //using Func to pass comparator method
-        var laptopsWithLessThan16Ram = delegateAndLinQ.FindLaptopByCriteriaUsingFunc(laptopList,
-            (laptop) => laptop.Motherboard.RamCount < 16);
-
-        //using extension method to get selling like title for laptop 
-        Console.WriteLine(laptopList[0].GetLaptopSellingTitle());
-
-        // using select and where on collection
-        var asusLaptopsWithMoreThan8Ram = laptopList.Where(laptop =>
-                laptop.Brand.Equals("ASUS", StringComparison.OrdinalIgnoreCase) && laptop.Motherboard.RamCount > 8)
-            .Select(laptop => new
-            {
-                laptop.Motherboard.Brand, laptop.Motherboard.CPU, laptop.Motherboard.RamCount,
-                laptop.Motherboard.SsdSize
-            });
+        return laptopList;
     }
 }
